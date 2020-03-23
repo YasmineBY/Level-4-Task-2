a858574c1cd89232314ec9c67098da4e4f9f1bb6
yasmine By <benyasminenl@gmail.com>
2020-03-23 10:49:44 +0100

    Begin/mockup of database

---
+++ Level4task2/app/src/main/java/com/example/level4_task2/model/Match.kt
@@ -0,0 +1,35 @@
+package com.example.level4_task2.model
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "match_table")
+data class Match (
+    // Match ID
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long? = null,
+
+      // Date
+     //todo add date entity
+
+    // Choice player
+    @ColumnInfo(name = "player_choice")
+    var chocicePlayer: String,
+
+    // Choice computer
+    @ColumnInfo(name = "computer_choice")
+    var computerChoice: String,
+
+    // Result
+    @ColumnInfo(name = "match_result")
+    var matchResult: String
+
+
+
+    ): Parcelable
+
