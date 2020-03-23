9cfcafbb2eff99176f25d1099a0088533f119f67
yasmine By <benyasminenl@gmail.com>
2020-03-23 11:27:11 +0100

    Layout_update

---
+++ Level4task2/app/src/main/java/com/example/level4_task2/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.level4_task2.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.level4_task2.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- Level4task2/app/src/main/java/com/example/level4_task2/ui/MatchHistory.kt
+++ Level4task2/app/src/main/java/com/example/level4_task2/ui/MatchHistory.kt
@@ -1,12 +1,13 @@
-package com.example.level4_task2
+package com.example.level4_task2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.level4_task2.R
 
-class MainActivity : AppCompatActivity() {
+class MatchHistory : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_match_history)
     }
 }
